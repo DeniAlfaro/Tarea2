1c903f4c7b71d7ad41ecf1c224c5ee780e5de86a
Deni <Deni@DENI>
2018-08-15 20:57:41 -0700

    Add project files.

---
+++ Program/Clase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Clase
+    {
+        public int PosicionX { get; set; }
+        public int PosicionY { get; set; }
+        public int Movimiento { get; set; }
+        private int Disparar { get; set; }
+        private int Explotar { get; set; }
+
+        private int _puntosGanados;
+        public int PuntosGanados
+        {
+            get
+            {
+                return _puntosGanados;
+            }
+            set
+            {
+                if (value < 100)
+                {
+                    Console.WriteLine("Inserte un valor mayor a 100");
+                }
+                else if (value >= 100) {
+                    Console.WriteLine("Puntos Ganados: " + PuntosGanados);
+                }
+            }
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clase enemigo = new Clase();
+            enemigo.PosicionX = 5;
+            enemigo.PosicionY = 6;
+            enemigo.PuntosGanados = 2;
+            enemigo.Movimiento = 3;
+
+            Console.WriteLine("Posicion en X: " + enemigo.PosicionX);
+            Console.WriteLine("Posicion en Y: " + enemigo.PosicionY);
+            Console.WriteLine("Total puntos: " + enemigo.PuntosGanados);
+            Console.WriteLine("Movimientos: " + enemigo.Movimiento);
+        }
+    }
+}
